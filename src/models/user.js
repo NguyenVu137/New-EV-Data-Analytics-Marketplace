cdb4e9c33c1d8b4747e0465c265fe527457814e8
Gia Hung <hungng2392@ut.edu.vn>
2025-11-04 17:01:50 +0700

    Fix code

--- src/models/user.js
+++ src/models/user.js
@@ -2,63 +2,41 @@
 const { Model, DataTypes } = require('sequelize')
 
 module.exports = (sequelize) => {
-    class User extends Model {
-        static associate(models) {
-            // FK với Allcode (role)
-            User.belongsTo(models.Allcode, { foreignKey: 'roleId', targetKey: 'key', as: 'role' })
-            // Quan hệ với các bảng khác
-            User.hasMany(models.Dataset, { foreignKey: 'provider_id', as: 'datasets' })
-            User.hasMany(models.Transaction, { foreignKey: 'consumer_id', as: 'transactions' })
-            User.hasMany(models.Subscription, { foreignKey: 'consumer_id', as: 'subscriptions' })
-            User.hasMany(models.ApiKey, { foreignKey: 'user_id', as: 'apiKeys' })
-            User.hasMany(models.Payout, { foreignKey: 'provider_id', as: 'payouts' })
-            User.hasMany(models.AuditLog, { foreignKey: 'user_id', as: 'auditLogs' })
-            User.hasMany(models.Notification, { foreignKey: 'user_id', as: 'notifications' })
-            User.hasMany(models.AdminActionLog, { foreignKey: 'admin_id', as: 'adminActions' })
-        }
+  class User extends Model {
+    static associate(models) {
+      // FK với Allcode (role)
+      User.belongsTo(models.Allcode, { foreignKey: 'roleId', targetKey: 'key', as: 'role' })
+      // Quan hệ với các bảng khác
+      User.hasMany(models.Dataset, { foreignKey: 'provider_id', as: 'datasets' })
+      User.hasMany(models.Transaction, { foreignKey: 'consumer_id', as: 'transactions' })
+      User.hasMany(models.Subscription, { foreignKey: 'consumer_id', as: 'subscriptions' })
+      User.hasMany(models.ApiKey, { foreignKey: 'user_id', as: 'apiKeys' })
+      User.hasMany(models.Payout, { foreignKey: 'provider_id', as: 'payouts' })
+      User.hasMany(models.AuditLog, { foreignKey: 'user_id', as: 'auditLogs' })
+      User.hasMany(models.Notification, { foreignKey: 'user_id', as: 'notifications' })
+      User.hasMany(models.AdminActionLog, { foreignKey: 'admin_id', as: 'adminActions' })
     }
-<<<<<<< HEAD
-  };
+  }
   User.init({
-    email: DataTypes.STRING,
-    password: DataTypes.STRING,
-    firstName: DataTypes.STRING,
-    lastName: DataTypes.STRING,
-    address: DataTypes.STRING,
-    phonenumber: DataTypes.STRING,
-    gender: DataTypes.BOOLEAN,
-    image: DataTypes.STRING,
-    roleId: DataTypes.STRING,
-    positionId: DataTypes.STRING,
+    id: { type: DataTypes.UUID, defaultValue: DataTypes.UUIDV4, primaryKey: true },
+    email: { type: DataTypes.STRING, allowNull: false, unique: true, validate: { isEmail: true } },
+    password: { type: DataTypes.STRING, allowNull: false },
+    firstName: { type: DataTypes.STRING, allowNull: true },
+    lastName: { type: DataTypes.STRING, allowNull: true },
+    address: { type: DataTypes.STRING, allowNull: true },
+    phonenumber: { type: DataTypes.STRING, allowNull: true },
+    gender: { type: DataTypes.STRING, allowNull: true },
+    image: { type: DataTypes.STRING, allowNull: true },
+    roleId: { type: DataTypes.STRING, allowNull: false },
+    positionId: { type: DataTypes.STRING, allowNull: true },
+    created_at: { type: DataTypes.DATE, defaultValue: DataTypes.NOW },
+    updated_at: { type: DataTypes.DATE, defaultValue: DataTypes.NOW }
   }, {
     sequelize,
     modelName: 'User',
-  });
-  return User;
-};
-=======
+    tableName: 'users',
+    timestamps: false
+  })
 
-    User.init({
-        id: { type: DataTypes.UUID, defaultValue: DataTypes.UUIDV4, primaryKey: true },
-        email: { type: DataTypes.STRING, allowNull: false, unique: true, validate: { isEmail: true } },
-        password: { type: DataTypes.STRING, allowNull: false },
-        firstName: { type: DataTypes.STRING, allowNull: true },
-        lastName: { type: DataTypes.STRING, allowNull: true },
-        address: { type: DataTypes.STRING, allowNull: true },
-        phonenumber: { type: DataTypes.STRING, allowNull: true },
-        gender: { type: DataTypes.STRING, allowNull: true },
-        image: { type: DataTypes.STRING, allowNull: true },
-        roleId: { type: DataTypes.STRING, allowNull: false },
-        positionId: { type: DataTypes.STRING, allowNull: true },
-        created_at: { type: DataTypes.DATE, defaultValue: DataTypes.NOW },
-        updated_at: { type: DataTypes.DATE, defaultValue: DataTypes.NOW }
-    }, {
-        sequelize,
-        modelName: 'User',
-        tableName: 'users',
-        timestamps: false
-    })
-
-    return User
+  return User
 }
->>>>>>> 9547f4b (Lưu tạm code để chuyển sang branch NGH)
